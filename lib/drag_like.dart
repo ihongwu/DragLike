1b9f19b8ba2f382cbd9c3117c4aa910cc1813c59
DoodleBear <yangmufeng233@gmail.com>
2021-02-27 00:25:10 +0800

    fix card up and down problem, fix when finger flit screen, touch twice for an instant can be treat as swipe out

--- lib/drag_like.dart
+++ lib/drag_like.dart
@@ -5,6 +5,7 @@ class DragLike extends StatefulWidget {
   final Widget secondChild;
   final double screenWidth;
   final double outValue;
+  final double dragSpeedRatio;
   final ValueChanged<String> onOutComplete;
   final VoidCallback onScaleComplete;
   final ValueChanged<Map> onChangeDragDistance;
@@ -15,6 +16,7 @@ class DragLike extends StatefulWidget {
       @required this.secondChild,
       @required this.screenWidth,
       @required this.outValue,
+      @required this.dragSpeedRatio,
       this.onOutComplete,
       this.onScaleComplete,
       this.onChangeDragDistance,
@@ -23,6 +25,7 @@ class DragLike extends StatefulWidget {
         assert(secondChild != null),
         assert(screenWidth != null),
         assert(outValue != null && outValue <= 1 && outValue > 0),
+        assert(dragSpeedRatio != null && dragSpeedRatio > 0),
         super(key: key);
 
   @override
@@ -35,7 +38,7 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
   // 第二个页面动画到前面
   Animation scaleAnimation;
   // 按下时的X坐标，以此判断是左滑还是右滑
-  double prevDx = 0;
+  double onPressDx = 0;
   // 拖拽时，上一瞬间x轴的位置
   double lastPositionX = 0;
   // 屏幕宽度
@@ -48,8 +51,6 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
   double distanceBetweenTwo = 0;
   // 拖拽发生的时间
   DateTime dragTime;
-  // 拖拽速率，超过该速率时松手会划出卡片
-  double dragSpeedRatio = 85;
   // 第二层的缩放值，0-0.1，因为已设置初始值为0.9
   double secondScale = 0;
   // 拖拽距离比0.0-1
@@ -65,19 +66,19 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
 
   // 更新偏移和缩放量
   void updatePosition(positionX) {
-    double offset = positionX - prevDx;
-    //上次两个瞬间的X轴距离
-    double lastDistance = (positionX - lastPositionX);
-    lastDistance = lastDistance.abs();
-    // 如果减速，重新开始计算时间
+    print("positionX = " + positionX.toString());
+    print("lastPositionX = " + lastPositionX.toString());
+    double offset = positionX - onPressDx;
+    print("offset = " + offset.toString());
+
     dragTime = DateTime.now();
-    // if (distanceBetweenTwo.abs() < lastDistance) {
-    // }
+
     distanceBetweenTwo = positionX - lastPositionX;
     print("distanceBetweenTwo = " + distanceBetweenTwo.toString());
     lastPositionX = positionX;
 
     double offsetAbs = offset.abs();
+    print("offsetAbs = " + offsetAbs.toString());
     double angleTemp =
         double.parse((offset / screenWidth / dragGvalue).toStringAsFixed(3));
     if (angle != angleTemp) {
@@ -85,7 +86,9 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
       secondScale = (offsetAbs / screenWidth / dragGvalue) / secondScaleSd;
       if (secondScale >= 0.1) secondScale = 0.1;
       dragDistance = offsetAbs / screenWidth;
-      if (distanceBetweenTwo < 0) {
+      print("dragDistance = " + dragDistance.toString());
+
+      if (offset < 0) {
         offsetX = -80;
       } else {
         offsetX = 80;
@@ -113,8 +116,7 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
         angle = this.animation.value;
         secondScale = angle.abs() / secondScaleSd;
         if (secondScale <= 0) secondScale = 0;
-        prevDx = 0;
-        lastPositionX = 0;
+
         dragDistance = 0;
         if (controller.isCompleted) {
           controller = null;
@@ -136,8 +138,7 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
             .animate(controller)
               ..addListener(() {
                 angle = this.animation.value;
-                prevDx = 0;
-                lastPositionX = 0;
+
                 dragDistance = 0;
                 if (controller.isCompleted) {
                   controller = null;
@@ -184,28 +185,34 @@ class _DragLikeState extends State<DragLike> with TickerProviderStateMixin {
         ),
         Listener(
           onPointerDown: (value) {
-            prevDx = value.position.dx;
+            onPressDx = value.position.dx;
+            lastPositionX = onPressDx;
             print("--------------------------------");
           },
           onPointerMove: (value) {
             updatePosition(value.position.dx);
           },
           onPointerUp: (value) async {
-            // 计算“手指抬起”瞬间和上一个“减速/速度波谷”的瞬间的时间差
             int timeOffset = DateTime.now().difference(dragTime).inMicroseconds;
             //滑动速率 = 两个瞬间的距离 / 两个瞬间的时间差
-            print("distanceBetweenTwo = " + distanceBetweenTwo.toString());
+            print("timeOffset = " + timeOffset.toString());
+            if (timeOffset < 5000)
+              timeOffset = 7500;
+            else if (timeOffset < 8000) timeOffset = 10000;
             double dragSpeed = (distanceBetweenTwo / timeOffset * 1e5).abs();
+
+            print("distanceBetweenTwo = " + distanceBetweenTwo.toString());
             print("dragSpeed = " + dragSpeed.toString());
-            if (dragDistance > widget.outValue || dragSpeed >= dragSpeedRatio) {
-              if (widget.onOutComplete != null) {
-                widget.onOutComplete(offsetX > 0 ? 'right' : 'left');
-              }
+            if (dragDistance > widget.outValue || dragSpeed >= widget.dragSpeedRatio) {
               if (dragDistance > widget.outValue) {
-                runOutAnimate(1); //以angle是否达到出界angle判断
+                //以angle是否达到出界angle判断
+                runOutAnimate(1); 
+                print(offsetX > 0 ? 'right' : 'left');
                 print("type: outValue");
               } else {
-                runOutAnimate(-1); //以两个瞬间滑动的方向来判断
+                //以两个瞬间滑动的方向来判断
+                runOutAnimate(-1); 
+                print(distanceBetweenTwo > 0 ? 'right' : 'left');
                 print("type: speed direction");
               }
               runInScaleAnimate();
